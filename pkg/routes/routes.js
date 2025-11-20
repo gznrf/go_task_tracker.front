4433be0be64a22c7bb448f46582478a2c639fb70
gznrf <iq703i@example.com>
2025-11-20 08:47:24 +0300

    feat()

--- pkg/routes/routes.js
+++ pkg/routes/routes.js
@@ -1,8 +1,10 @@
+
+const defaultRoute = "http://195.133.77.31:3321/"
 export const pagesRoutes = {
-    "/login": "http://localhost:5173/pkg/internal/auth/login/page.html",
-    "/register": "http://localhost:5173/pkg/internal/auth/register/page.html",
-    "/projects": "http://localhost:5173/pkg/internal/project/all_projects/page.html",
-    "/create-project": "http://localhost:5173/pkg/internal/project/create_project/page.html",
+    "/login": defaultRoute+"pkg/internal/auth/login/page.html",
+    "/register": defaultRoute+"pkg/internal/auth/register/page.html",
+    "/projects": defaultRoute+"pkg/internal/project/all_projects/page.html",
+    "/create-project":  defaultRoute+"pkg/internal/project/create_project/page.html",
     "/project":"../internal/",
     "/boards": "../internal/",
     "/board": "../internal/",

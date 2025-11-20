98b2357d2e65207a2d9e29917e2c22df7e56e2f2
gznrf <iq703i@example.com>
2025-11-20 06:43:26 +0300

    feat(back port)

--- pkg/api/routes/routes.js
+++ pkg/api/routes/routes.js
@@ -5,7 +5,7 @@ import {boardRoutes} from './entities/board.js';
 import {columnRoutes} from './entities/column.js';
 import {taskRoutes} from './entities/task.js';
 
-export const defaultRoute = "http://localhost:2002"
+export const defaultRoute = "http://localhost:8231"
 
 export const entitiesRoutes = {
     authRoutes,

7ec36ffc1f0e3e056e1c88a3d416cd90ab827001
gznrf <iq703i@example.com>
2025-12-16 02:31:37 +0300

    fix(): CORS

--- pkg/api/requests/entities/auth.js
+++ pkg/api/requests/entities/auth.js
@@ -2,7 +2,7 @@ import BaseRequest from "./base.js";
 import {entitiesRoutes} from "../../routes/routes.js";
 
 export const AuthRequests = {
-    register: (data) => BaseRequest.post(entitiesRoutes.authRoutes.SignUp, data),
-    login: (data) => BaseRequest.post(entitiesRoutes.authRoutes.SignIn, data),
-    logout: (data) => BaseRequest.post(entitiesRoutes.authRoutes.Logout, data),
+    register: (data) => BaseRequest.post(entitiesRoutes.authRoutes.SignUp, JSON.stringify(data)),
+    login: (data) => BaseRequest.post(entitiesRoutes.authRoutes.SignIn, JSON.stringify(data)),
+    logout: (data) => BaseRequest.post(entitiesRoutes.authRoutes.Logout, JSON.stringify(data)),
 };
